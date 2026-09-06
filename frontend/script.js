6e14c8845e7ec95aeda3e8ec7f729da3b4a72e3c
Chitr Batala <chitrbatala@gmail.com>
2026-09-06 22:40:25 +0530

    Use relative API endpoint for deployment

--- frontend/script.js
+++ frontend/script.js
@@ -138,7 +138,7 @@ form.addEventListener("submit", async function (event) {
         // ====================================
 
         const response = await fetch(
-            "http://127.0.0.1:8000/predict",
+            "/predict",
             {
                 method: "POST",
 
